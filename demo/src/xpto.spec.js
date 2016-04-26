da8d1e340debbaa84696ee47cee0211b78943106
Carlos Henrique <carloshpds@gmail.com>
2016-04-26 18:22:21 -0300

    updating bb-component at demo

---
+++ demo/src/xpto.spec.js
@@ -0,0 +1,13 @@
+
+// let chai = require('chai');
+import chai from 'chai';
+
+const assert = chai.assert;
+
+describe('when test xpto', function () {
+
+  it('is true', function () {
+    assert.isTrue(true);
+  });
+
+});
