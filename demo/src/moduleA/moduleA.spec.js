3bb5590ad72e4041b82d959f06f8c0c2db6b6f2f
Bruno Fachine <bdfachine@gmail.com>
2017-10-19 21:06:26 -0200

    Fix moduleA unit tests

--- demo/src/moduleA/moduleA.spec.js
+++ demo/src/moduleA/moduleA.spec.js
@@ -7,7 +7,7 @@ describe('Array',  function()  {
       describe('Line 4', function () {
         describe('Line 5', function () {
           it('should return -1 when the value is not present', function () {
-            assert.equal(-1, [1,2,3].indexOf(1));
+            assert.equal(0, [1,2,3].indexOf(1));
             assert.equal(-1, [1,2,3].indexOf(0));
           });
           it('should return -1 when the value is not present 2', function () {
