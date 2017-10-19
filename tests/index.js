59c310a8b82bc645767e62025f33f059bcf3d67a
Bruno Fachine <bdfachine@gmail.com>
2017-10-19 21:11:33 -0200

    Rename tests contexts

--- tests/index.js
+++ tests/index.js
@@ -1,12 +1,8 @@
-// if(typeof window != 'undefined'){
-//   window.mocha.setup( {ui: 'tdd'} );
-//   require("mocha/mocha.css");
-// }
+var tests = require.context("$projectRoot/src", true, /.+\.spec\.js$/);
+tests.keys().forEach(tests);
 
-var context2 = require.context("$projectRoot/src", true, /.+(\.jsx?|\.coffee)$/);
-context2.keys().forEach(context2);
+var components = require.context("$projectRoot/src", true, /.+(\.jsx?|\.coffee)$/);
+components.keys().forEach(components);
 
-var context = require.context("$projectRoot/src", true, /.+\.spec\.js$/);
-context.keys().forEach(context);
 
-module.exports = context;
+module.exports = tests;
