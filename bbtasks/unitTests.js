b215fec13d47d77612ccdedba028410ff0c0fccd
Bruno Fachine <bdfachine@gmail.com>
2017-10-20 16:05:37 -0200

    Setup gulp test/test:coverage

--- bbtasks/unitTests.js
+++ bbtasks/unitTests.js
@@ -7,10 +7,14 @@ module.exports = function(options){
         path                   = require('path'),
         cross_env              = path.resolve(`${__dirname}/../node_modules/.bin/cross-env`),
         mocha_webpack          = path.resolve(`${__dirname}/../node_modules/.bin/mocha-webpack`),
-        spec_files             = path.resolve(`${process.cwd()}/./src/**/*.spec.*`),
+        spec_files             = path.resolve(`${process.cwd()}/./src/**/*.spec.{js,coffee}`),
         nyc_output_folder_path = path.resolve(`${process.cwd()}/./coverage/.nyc_output`);
 
   gulp.task('test', shell.task(
-    `${cross_env} NODE_ENV=testing nyc --temp-directory='${nyc_output_folder_path}' --instrument=false --source-map=false --include='src/**/!(*.spec).*' --reporter=lcov --reporter=text --report-dir='${process.cwd()}/coverage' ${mocha_webpack} --webpack-config ../scripts/webpack.tests.config.js '${spec_files}'`
+    `${cross_env} NODE_ENV=testing ${mocha_webpack} --webpack-config ../scripts/webpack.tests.config.js '${spec_files}'`
+  ));
+
+  gulp.task('test:coverage', shell.task(
+    `${cross_env} NODE_ENV=testing nyc --temp-directory='${nyc_output_folder_path}' --instrument=false --source-map=false --include='src/**/!(*.spec).{js,coffee}' --reporter=lcov --reporter=text --report-dir='${process.cwd()}/coverage' ${mocha_webpack} --webpack-config ../scripts/webpack.tests.config.js '${spec_files}'`
   ));
 };
