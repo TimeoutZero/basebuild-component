6ad6a7c1c351a7ce1f92f17ca0327d68f77340de
BrunoDF <bdfachine@gmail.com>
2017-10-23 13:32:39 -0200

    JSDOM-global

--- bbtasks/unitTests.js
+++ bbtasks/unitTests.js
@@ -8,19 +8,18 @@ module.exports = function(options){
         mocha_webpack          = path.resolve(require.resolve('mocha-webpack').replace(/node_modules(.*)$/, 'node_modules/.bin/mocha-webpack')),
         nyc                    = path.resolve(require.resolve('nyc').replace(/node_modules(.*)$/, 'node_modules/.bin/nyc')),
         webpack_config         = path.resolve(`${__dirname}/../scripts/webpack.tests.config.js`),
-        js_dom_config          = path.resolve(`${__dirname}/../scripts/jsdom.setup.js`),
         spec_files             = path.resolve(`${process.cwd()}/./src/**/*.spec.{js,coffee}`),
         nyc_output_folder_path = path.resolve(`${process.cwd()}/./coverage/.nyc_output`);
 
   gulp.task('test', shell.task(
-    `${cross_env} NODE_ENV=testing ${mocha_webpack} --require ${js_dom_config} --webpack-config ${webpack_config} "${spec_files}"`
+    `${cross_env} NODE_ENV=testing ${mocha_webpack} --require jsdom-global/register --webpack-config ${webpack_config} "${spec_files}"`
   ));
 
   gulp.task('test:auto', shell.task(
-    `${cross_env} NODE_ENV=testing ${mocha_webpack} --require ${js_dom_config} --watch --webpack-config ${webpack_config} "${spec_files}"`
+    `${cross_env} NODE_ENV=testing ${mocha_webpack} --require jsdom-global/register --watch --webpack-config ${webpack_config} "${spec_files}"`
   ));
 
   gulp.task('test:coverage', shell.task(
-    `${cross_env} NODE_ENV=testing ${nyc} --temp-directory="${nyc_output_folder_path}" --instrument=false --source-map=false --include="src/**/!(*.spec).{js,coffee}" --reporter=lcov --reporter=text --report-dir="${process.cwd()}/coverage" ${mocha_webpack} --require ${js_dom_config} --webpack-config ${webpack_config} "${spec_files}"`
+    `${cross_env} NODE_ENV=testing ${nyc} --temp-directory="${nyc_output_folder_path}" --instrument=false --source-map=false --include="src/**/!(*.spec).{js,coffee}" --reporter=lcov --reporter=text --report-dir="${process.cwd()}/coverage" ${mocha_webpack} --require jsdom-global/register --webpack-config ${webpack_config} "${spec_files}"`
   ));
 };
